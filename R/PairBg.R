aeea80e9265e7d7bec643cdf8aad4da975130302
alvinlin0824 <alvin.lin@abbott.com>
2023-08-09 13:08:01 -0700

    Commit From RStudio

--- R/PairBg.R
+++ R/PairBg.R
@@ -1,36 +1,36 @@
 #' Pair BG Reference
 #'
-#' @param data Upload Data
+#' @param sensor_data Sensor Data
 #'
 #' @return Tibble
 #' @export
 
-PairBg <- function(data){
+PairBg <- function(sensor_data){
   # BG Strips Data
   BG |>
     # Remove Extreme Reference readings <20 or >500
-    dplyr::filter(dplyr::between(Reference,20,500)) |>
-    dplyr::inner_join(
+    filter(between(Reference,20,500)) |>
+    inner_join(
       # Sensor Data
-      data |>
+      sensor_data |>
         dplyr::mutate(
-          `Lower Bound` = dplyr::case_when(Type %in% c("904","906") ~ `Date Time` - lubridate::dminutes(5),
-                                    .default = `Date Time` - lubridate::dminutes(8)),
-          `Upper Bound` = dplyr::case_when(Type %in% c("904","906") ~ `Date Time` + lubridate::dminutes(5),
-                                    .default = `Date Time` + lubridate::dminutes(8))
+          `Lower Bound` = case_when(Type %in% c("904","906") ~ `Date Time` - dminutes(5),
+                                    .default = `Date Time` - dminutes(8)),
+          `Upper Bound` = case_when(Type %in% c("904","906") ~ `Date Time` + dminutes(5),
+                                    .default = `Date Time` + dminutes(8))
         ),
-      by = dplyr::join_by("Subject ID",dplyr::between(`BG Date Time`,`Lower Bound`,`Upper Bound`)),
+      by = join_by("Subject ID",between(`BG Date Time`,`Lower Bound`,`Upper Bound`)),
       multiple = "all"
     ) |>
     # Select the nearest time
-    dplyr::group_by(`Subject ID`,`BG Date Time`) |>
-    dplyr::arrange(desc(`Date Time`)) |>
-    dplyr::slice_min(abs(`BG Date Time` - `Date Time`),n = 1,with_ties = F) |>
-    dplyr::ungroup() |>
+    group_by(`Subject ID`,`BG Date Time`) |>
+    arrange(desc(`Date Time`)) |>
+    slice_min(abs(`BG Date Time` - `Date Time`),n = 1,with_ties = F) |>
+    ungroup() |>
     # Select the nearest time
-    dplyr::group_by(`Subject ID`,`Date Time`) |>
-    dplyr::arrange(desc(`Date Time`)) |>
-    dplyr::slice_min(abs(`BG Date Time` - `Date Time`),n = 1,with_ties = F) |>
-    dplyr::ungroup() |>
-    dplyr::select(!c(contains("Bound")))
+    group_by(`Subject ID`,`Date Time`) |>
+    arrange(desc(`Date Time`)) |>
+    slice_min(abs(`BG Date Time` - `Date Time`),n = 1,with_ties = F) |>
+    ungroup() |>
+    select(!c(contains("Bound")))
 }
