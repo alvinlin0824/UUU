37dd5777b73ea5da18c7e08dd1f298b00e72ab37
alvinlin0824 <alvin.lin@abbott.com>
2023-12-06 08:12:52 -0800

    Commit From RStudio

--- R/filter_path.R
+++ R/filter_path.R
@@ -7,22 +7,22 @@
 #' @export
 
 filter_path <- function(file_path, filter_text = "^$|pattern"){
-  events_path <- file_path[stringr::str_detect(file_path,"events") &
+  events_path <- file_path[stringr::str_detect(file_path,"events.csv") &
                              !stringr::str_detect(file_path,stringr::regex("Archive|Archives|Transfer|Transfers",ignore_case = T)) & !stringr::str_detect(file_path,stringr::regex(filter_text,ignore_case = T))]
 
-  gluc_path <- file_path[stringr::str_detect(file_path,"gluc") &
+  gluc_path <- file_path[stringr::str_detect(file_path,"gluc.csv") &
                            !stringr::str_detect(file_path,stringr::regex("Archive|Archives|Transfer|Transfers",ignore_case = T)) & !stringr::str_detect(file_path,stringr::regex(filter_text,ignore_case = T))]
 
-  glucPlus_path <- file_path[stringr::str_detect(file_path,"glucPlus") &
+  glucPlus_path <- file_path[stringr::str_detect(file_path,"glucPlus.csv") &
                                !stringr::str_detect(file_path,stringr::regex("Archive|Archives|Transfer|Transfers",ignore_case = T)) & !stringr::str_detect(file_path,stringr::regex(filter_text,ignore_case = T))]
 
-  free_path <- file_path[stringr::str_detect(file_path,stringr::regex("freestyle",ignore_case = T)) &
+  free_path <- file_path[stringr::str_detect(file_path,stringr::regex("freestyle.csv",ignore_case = T)) &
                            !stringr::str_detect(file_path,stringr::regex("Archive|Archives|Transfer|Transfers",ignore_case = T)) & !stringr::str_detect(file_path,stringr::regex(filter_text,ignore_case = T))]
 
-  anaPlus_path <- file_path[stringr::str_detect(file_path,stringr::regex("anaPlus",ignore_case = T)) &
+  anaPlus_path <- file_path[stringr::str_detect(file_path,stringr::regex("anaPlus.csv",ignore_case = T)) &
                            !stringr::str_detect(file_path,stringr::regex("Archive|Archives|Transfer|Transfers",ignore_case = T)) & !stringr::str_detect(file_path,stringr::regex(filter_text,ignore_case = T))]
 
-  oneMinutes_path <- file_path[stringr::str_detect(file_path,stringr::regex("oneMinutes",ignore_case = T)) &
+  oneMinutes_path <- file_path[stringr::str_detect(file_path,stringr::regex("oneMinutes.csv",ignore_case = T)) &
                               !stringr::str_detect(file_path,stringr::regex("Archive|Archives|Transfer|Transfers",ignore_case = T)) & !stringr::str_detect(file_path,stringr::regex(filter_text,ignore_case = T))]
 
   out <- list(events_path, gluc_path, glucPlus_path, free_path, anaPlus_path, oneMinutes_path)
