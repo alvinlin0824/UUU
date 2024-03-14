fa00ba8ce65bd11ad6df4726decf5b5645e90171
alvinlin0824 <alvin.lin@abbott.com>
2024-03-14 14:47:40 -0700

    Diff Measure

--- R/diff_measure.R
+++ R/diff_measure.R
@@ -1,16 +1,19 @@
 #' Difference Measures Analysis
 #'
 #' @param data Paired Data Set
-#' @param group_var Any Categorical Variables
-#' @param ref Provide Reference Readings such as YSI or BG
+#' @param type Either Glucose or Ketone. Case is not sensitive
+#' @param ref Provide Reference Readings
 #' @param cgm Provide Sensor Readings such as Gl or ANA
+#' @param breakpoint It depends on which type and unit. Default is 100
+#' @param col_subject Provide Subject Column
+#' @param group_var Any Categorical Variables
 #' @param unit Either mg/dL or mmol/L. Default is mg/dL
 #' @param reference_breakpoint Default is TRUE
-#'
+
 #' @return Gt Table
 #' @export
 
-diff_measure <- function(data, ref, cgm, group_var, unit = "mg/dL", reference_breakpoint = TRUE) {
+diff_measure  <- function(data, type = "Glucose" ,ref, cgm, breakpoint = 100, col_subject, group_var, unit = "mg/dL", reference_breakpoint = TRUE) {
 
   round_normal <- function(x, digits){
     ifelse(x >= 0,round(x + 10^-(digits + 3),digits),round(x - 10^-(digits + 3),digits))
@@ -23,38 +26,85 @@ diff_measure <- function(data, ref, cgm, group_var, unit = "mg/dL", reference_br
                            Max = ~ max(.x, na.rm = T),
                            N = ~ sum(!is.na(.x),na.rm = T))
   df <- data |>
-          select(!contains("Difference")) |>
-          mutate(Difference = {{cgm}} - {{ref}},
-                `Absolute Difference` = abs(Difference),
-                `Relative Difference(%)` = (Difference/{{ref}})*100,
-                `Absolute Relative Difference(%)` = abs(`Relative Difference(%)`))
+    select(!contains("Difference")) |>
+    mutate(Subject = as.character({{col_subject}}),
+           Difference = {{cgm}} - {{ref}},
+           `Absolute Difference` = abs(Difference),
+           `Relative Difference(%)` = (Difference/{{ref}})*100,
+           `Absolute Relative Difference(%)` = abs(`Relative Difference(%)`))
 
 
 
   if (reference_breakpoint == TRUE) {
-    # gt_group_var <- c("Level",group_var)
-    df |>
-      mutate(# Reference Level
-        Level = if (unit == "mg/dL") {
-          case_when(round({{ref}} + 0.001) < 100 ~ "<100 mg/dL[5.6 mmol/L]",
-                    .default = ">=100 mg/dL[5.6 mmol/L]")}
+    # Glucose
+    if (str_detect(type,regex("Glucose",ignore_case = T))) {
+      lower_label <- if_else(unit == "mg/dL",
+                             str_c("<",breakpoint," mg/dL[",round_normal(breakpoint/18.016,1)," mmol/L]"),
+                             str_c("<",floor(breakpoint*18.016)," mg/dL[",breakpoint," mmol/L]")
+      )
+      upper_label <- if_else(unit == "mg/dL",
+                             str_c(">=",breakpoint," mg/dL[",round_normal(breakpoint/18.016,1)," mmol/L]"),
+                             str_c(">=",floor(breakpoint*18.016)," mg/dL[",breakpoint," mmol/L]")
+      )
+      overall_label <- if_else(unit == "mg/dL",
+                               str_c("Overall Levels (",breakpoint," mg/dL[",round_normal(breakpoint/18.016,1)," mmol/L] breakpoint)"),
+                               str_c("Overall Levels (",floor(breakpoint*18.016)," mg/dL[",breakpoint," mmol/L] breakpoint)")
+      )
+    }
+    # Ketone
+    else if (str_detect(type,regex("Ketone",ignore_case = T))) {
+      lower_label <- str_c("<",breakpoint," mmol/L")
+      upper_label <- str_c(">=",breakpoint," mmol/L")
+      overall_label <- str_c("Overall Levels (",breakpoint," mmol/L breakpoint)")
+    }
+
+    df1 <- df |>
+      # Reference Level
+      mutate(Level = if (str_detect(type,regex("Glucose",ignore_case = T))){
+        if (unit == "mg/dL") {
+          case_when(round({{ref}} + 0.001) < breakpoint ~ lower_label,
+                    .default = upper_label)}
         else if (unit == "mmol/L"){
-          case_when(round({{ref}} + 0.001,1) < 5.6 ~ "<100 mg/dL[5.6 mmol/L]",
-                    .default = ">=100 mg/dL[5.6 mmol/L]")
-        })|>
+          case_when(round({{ref}} + 0.001,1) < breakpoint ~ lower_label,
+                    .default = upper_label)}}
+        else if (str_detect(type,regex("Ketone",ignore_case = T))){
+          case_when({{ref}} < breakpoint ~ lower_label,
+                    .default = upper_label)
+        }
+      ) |>
       # Overall Reference Level
       bind_rows(df |>
-                  mutate(Level = "Overall Levels (100 mg/dL[5.6 mmol/L] breakpoint)")) |>
+                  mutate(Level = overall_label)) |>
       pivot_longer(Difference:`Absolute Relative Difference(%)`,names_to = "Measure") |>
       filter((Measure %in% c("Difference", "Absolute Difference") &
-                Level %in% c("<100 mg/dL[5.6 mmol/L]", "Overall Levels (100 mg/dL[5.6 mmol/L] breakpoint)")) |
+                Level %in% c(lower_label, overall_label)) |
                (Measure %in% c("Relative Difference(%)", "Absolute Relative Difference(%)") &
-                  Level %in% c(">=100 mg/dL[5.6 mmol/L]", "Overall Levels (100 mg/dL[5.6 mmol/L] breakpoint)"))) |>
-      group_by(pick(Level,Measure)) |>
+                  Level %in% c(upper_label, overall_label))) |>
+      group_by(pick(Level,Measure))
+
+    # Diff Measure
+    df1 |>
       # User-Defined group variables
       group_by(pick({{group_var}}),.add = TRUE) |>
       summarise_at(.vars = "value", diff_calculation) |>
       ungroup() |>
+      left_join(
+        # Confidence Interval
+        df1 |>
+          # User-Defined group variables `Type of Diabetes`
+          group_by(pick({{group_var}}),.add = TRUE) |>
+          # Need to think second
+          group_by(Subject, .add = TRUE) |>
+          summarise(bias_mean_subj = mean(value, na.rm = T),.groups = "drop_last") |>
+          summarise(bias_mean = mean(bias_mean_subj, na.rm = T),
+                    bias_sd = sd(bias_mean_subj, na.rm = T)/n()**0.5,
+                    N = n(),.groups = "drop") |>
+          mutate(`Grand Mean (95% CI)` = str_c(round_normal(bias_mean,1), " (",
+                                               round_normal(bias_mean - qt(p = 0.025, df = N - 1, lower.tail = F) * bias_sd, digits = 1),
+                                               ",",
+                                               round_normal(bias_mean + qt(p = 0.025, df = N - 1, lower.tail = F) * bias_sd, digits = 1),
+                                               ")"),.keep = "unused")) |>
+      relocate(`Grand Mean (95% CI)`,.before = N) |>
       # Round Number
       mutate(across(c(Mean:Max), ~ round_normal(.x,1))) |>
       # Unit
@@ -65,7 +115,7 @@ diff_measure <- function(data, ref, cgm, group_var, unit = "mg/dL", reference_br
         ~ str_c(Measure," (",unit,")"),
         .default = Measure
       ),
-      Level = factor(Level, levels = c("<100 mg/dL[5.6 mmol/L]",">=100 mg/dL[5.6 mmol/L]", "Overall Levels (100 mg/dL[5.6 mmol/L] breakpoint)")),
+      Level = factor(Level, levels = c(lower_label,upper_label, overall_label)),
       Measure = factor(Measure, levels = c("Difference (mg/dL)","Absolute Difference (mg/dL)","Difference (mmol/L)","Absolute Difference (mmol/L)","Relative Difference(%)","Absolute Relative Difference(%)"))) |>
       arrange(pick(Level,Measure)) |>
       arrange(pick({{group_var}})) |>
@@ -73,7 +123,8 @@ diff_measure <- function(data, ref, cgm, group_var, unit = "mg/dL", reference_br
       gt::cols_align(align = "center") |>
       gt::fmt_number(columns = Mean:Max,decimals = 1) |>
       gt::sub_missing(columns = everything(),missing_text = "") |>
-      gt::opt_stylize(style = 6, color = "blue")
+      gt::opt_stylize(style = 6, color = "blue") |>
+      suppressMessages()
   } else {
     gt_group_var <- group_var[1:length(group_var) - 1]
     df |>
