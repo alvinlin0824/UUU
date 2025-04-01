5c735a9195278a20360a58fd55f8327dbdf0b5d2
alvinlin0824 <alvin.lin@abbott.com>
2025-04-01 11:18:13 -0700

    Updated Aanplus.csv

--- R/mobi_anaPlus.R
+++ R/mobi_anaPlus.R
@@ -9,8 +9,38 @@
 
 mobi_anaPlus <- function(events, ana, index = NULL) {
 
+  # Check packages
+  packages <- c("tidyverse","vroom","rlang","data.table")
+  for (package_name in packages) {
+    if (!require(package_name, character.only = TRUE)) {
+      install <- readline(prompt = paste("Package", package_name, "is not installed. Do you want to install it? (Yes/No): "))
+      # If say yes
+      if (tolower(install) == "yes") {
+        install.packages(package_name)
+        library(package_name, character.only = TRUE)
+        message(paste("Package", package_name, "has been installed and loaded."))
+      }
+      # If say no
+      else {
+        message(paste("Package", package_name, "was not installed."))
+      }
+    }
+    #   else {
+    #   message(paste("Package", package_name, "is already installed and loaded."))
+    # }
+  }
+
+
+  if (length(events) != length(ana)) {rlang::abort("Number of events.csv is not equal to number of anaPlus.csv!")}
+
+  if (!is.numeric(index) && !is.null(index)) {rlang::abort("Index must be numeric!")}
+
   # Individual File
   if (is.numeric(index)) {
+
+    if (index > length(events) || index > length(ana)) {rlang::abort("Index beyond the range!")}
+
+    else {
     purrr::map2(
       # First List
       # Import Events
@@ -30,7 +60,7 @@ mobi_anaPlus <- function(events, ana, index = NULL) {
         ana[index] |>
           purrr::map(purrr::possibly(\(path) vroom::vroom(path,delim = ",",col_names = T,show_col_types = F,col_types = c(Date = "c", Time = "c", Type = "c"),col_select = c(`Unique Record ID`,Date,Time,Type,ANA,Tr),n_max = 2),tibble::tibble()),.progress = TRUE),
         ana[index] |>
-          purrr::map(purrr::possibly(\(path) data.table::fread(path,select = c(1:5,7),skip = 3,col.names = c("Unique Record ID","Date","Time","Type","ANA","Tr"),colClasses = c("V2" = "character","V3" = "character","V4" = "character")),tibble::tibble()),.progress = TRUE),
+          purrr::map(purrr::possibly(\(path) data.table::fread(path,select = c(1:7),skip = 3,col.names = c("Unique Record ID","Date","Time","Type","ANA","Rate","Tr"),colClasses = c("V2" = "character","V3" = "character","V4" = "character")),tibble::tibble()),.progress = TRUE),
         dplyr::bind_rows,.progress = TRUE) |>
         purrr::map(\(df) df |> dplyr::transmute(`Subject ID` =
                                                   dplyr::case_when(
@@ -51,18 +81,20 @@ mobi_anaPlus <- function(events, ana, index = NULL) {
                                                 `Date Time` = lubridate::ymd_hms(stringr::str_c(Date,Time,sep = " ")),
                                                 Type = Type,
                                                 ANA = ANA,
+                                                Rate = Rate,
                                                 Tr = Tr),.progress = TRUE) |>
-      purrr::map(\(df) df |> dplyr::slice(2:n())),dplyr::bind_rows,.progress = TRUE) |>
+        purrr::map(\(df) df |> dplyr::slice(2:n())),dplyr::bind_rows,.progress = TRUE) |>
       purrr::map(\(df) df |> dplyr::arrange(`Date Time`),.progress = TRUE) |>
       purrr::map(\(df) df |> tidyr::fill(c(`Subject ID`,`Condition ID`),.direction = "up"),.progress = TRUE) |>
       purrr::map(\(df) df |> tidyr::fill(`Sensor Serial Number`,.direction = "down"),.progress = TRUE) |>
       purrr::map(\(df) df |> dplyr::filter(!(!is.na(`Sensor Serial Number`) & is.na(`Date Time`))),.progress = TRUE) |>
       purrr::map(\(df) df |> dplyr::relocate(`Subject ID`,`Condition ID`,`Sensor Serial Number`,
-                                             `Date Time`,Type,ANA,Tr),.progress = TRUE) |>
+                                             `Date Time`,Type,ANA,Rate,Tr),.progress = TRUE) |>
       purrr::list_rbind(names_to = "Path") |>
       suppressWarnings()
-
-  } else {
+   }
+  }
+    else if (is.null(index)) {
 
     # All Upload Data
     purrr::map2(
@@ -84,7 +116,7 @@ mobi_anaPlus <- function(events, ana, index = NULL) {
         ana |>
           purrr::map(purrr::possibly(\(path) vroom::vroom(path,delim = ",",col_names = T,show_col_types = F,col_types = c(Date = "c", Time = "c", Type = "c"),col_select = c(`Unique Record ID`,Date,Time,Type,ANA,Tr),n_max = 2),tibble::tibble()),.progress = TRUE),
         ana |>
-          purrr::map(purrr::possibly(\(path) data.table::fread(path,select = c(1:5,7),skip = 3,col.names = c("Unique Record ID","Date","Time","Type","ANA","Tr"),colClasses = c("V2" = "character","V3" = "character","V4" = "character")),tibble::tibble()),.progress = TRUE),
+          purrr::map(purrr::possibly(\(path) data.table::fread(path,select = c(1:7),skip = 3,col.names = c("Unique Record ID","Date","Time","Type","ANA","Rate","Tr"),colClasses = c("V2" = "character","V3" = "character","V4" = "character")),tibble::tibble()),.progress = TRUE),
         dplyr::bind_rows,.progress = TRUE) |>
         purrr::map(\(df) df |> dplyr::transmute(`Subject ID` =
                                                   dplyr::case_when(
@@ -105,6 +137,7 @@ mobi_anaPlus <- function(events, ana, index = NULL) {
                                                 `Date Time` = lubridate::ymd_hms(stringr::str_c(Date,Time,sep = " ")),
                                                 Type = Type,
                                                 ANA = ANA,
+                                                Rate = Rate,
                                                 Tr = Tr),.progress = TRUE) |>
       purrr::map(\(df) df |> dplyr::slice(2:n())),dplyr::bind_rows,.progress = TRUE) |>
       purrr::map(\(df) df |> dplyr::arrange(`Date Time`),.progress = TRUE) |>
@@ -112,7 +145,7 @@ mobi_anaPlus <- function(events, ana, index = NULL) {
       purrr::map(\(df) df |> tidyr::fill(`Sensor Serial Number`,.direction = "down"),.progress = TRUE) |>
       purrr::map(\(df) df |> dplyr::filter(!(!is.na(`Sensor Serial Number`) & is.na(`Date Time`))),.progress = TRUE) |>
       purrr::map(\(df) df |> dplyr::relocate(`Subject ID`,`Condition ID`,`Sensor Serial Number`,
-                                             `Date Time`,Type,ANA,Tr),.progress = TRUE) |>
+                                             `Date Time`,Type,ANA,Rate,Tr),.progress = TRUE) |>
       purrr::list_rbind(names_to = "Path") |>
       # Remove Duplicated Uploads
       # dplyr::distinct() |>
